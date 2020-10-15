3293352eb4ad7bc943745c1dc5bc20904fd85fd8
SexyM <48942066+SexyMg@users.noreply.github.com>
2020-10-15 18:26:46 +0900

    고침

--- Multy_Isaac/Assets/Resources/Sources/Scripts/ItemSlot.cs
+++ Multy_Isaac/Assets/Resources/Sources/Scripts/ItemSlot.cs
@@ -6,13 +6,22 @@ using UnityEngine.UI;
 
 public class ItemSlot : MonoBehaviour
 {
-    public PlayerItem player;
+    PlayerItem player;
     public int index;
     private Image img;
     public GameObject Btns;
     private void Start()
     {
         img = GetComponent<Image>();
+        Player[] players = FindObjectsOfType<Player>();
+        foreach (Player p in players)
+        {
+            if (p.pv.IsMine)
+            {
+                player = p.GetComponent<PlayerItem>();
+                break;
+            }
+        }  
     }
 
     public void Drag()
