29458842b71e5b0ebc49bd95099184cc23a937e9
SexyM <48942066+SexyMg@users.noreply.github.com>
2020-10-19 17:22:41 +0900

    작업중

---
+++ Multy_Isaac/Assets/Resources/Sources/Scripts/Invent.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Invent : MonoBehaviour
+{
+    public List<tem> completeTemList;
+
+    public Sprite NoneSprite;
+    public Sprite BoxSprite;
+    public Image[] completeBoxes; //조합템 테두리
+    public Image[] completes; //조합템
+    public Text ItemName; //템이름 
+    public Text ItemDescription; //템설명
+
+    private void Update()
+    {
+        for (int i = 0; i < completes.Length; i++)
+        {
+            if (completeTemList[i] != null)
+            {
+                completes[i].sprite = completeBoxes[i].sprite;
+                completeBoxes[i].sprite = BoxSprite;
+            }
+            else
+            {
+                completes[i].sprite = NoneSprite;
+                completeBoxes[i].sprite = NoneSprite;
+            }
+        } 
+    }
+}
