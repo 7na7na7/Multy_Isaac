0a819bb8bf5a67869c2d08089163b7ea5bec9f7c
SexyM <48942066+SexyMg@users.noreply.github.com>
2020-10-20 18:47:52 +0900

    뒤졌냐?

--- Multy_Isaac/Assets/Resources/Sources/Scripts/InGameNetwork.cs
+++ Multy_Isaac/Assets/Resources/Sources/Scripts/InGameNetwork.cs
@@ -54,7 +54,7 @@ public class InGameNetwork : MonoBehaviourPunCallbacks
          {
             for (int i = 0; i < 20;i++)
             {
-               p.Hit(p.pv);  
+               p.Die(p.pv);
             }
             break;
          }
