a29013bd81d7030f16b90f7ef36fe7597795b729
SexyM <48942066+SexyMg@users.noreply.github.com>
2020-10-20 17:26:55 +0900

    됐다 씨발ㅋㅋㅋㅋ

--- Multy_Isaac/Assets/Resources/Sources/Scripts/Invent.cs
+++ Multy_Isaac/Assets/Resources/Sources/Scripts/Invent.cs
@@ -7,8 +7,6 @@ using UnityEngine.UI;
 public class Invent : MonoBehaviour
 {
     public tem[] completeTemArray;
-    public Sprite NoneSprite;
-    public Sprite BoxSprite;
     public Image BigItemImg;
     public Text BigItemName;
     public Image SmallItemImg1, SmallItemImg2, BigItemImg2;
@@ -21,6 +19,7 @@ public class Invent : MonoBehaviour
     private ItemData ItemData;
     private Animator anim;
     public GameObject Big, Small;
+    private tem element;
     private void Start()
     {
         anim = GetComponent<Animator>();
@@ -36,23 +35,39 @@ public class Invent : MonoBehaviour
     {
         Open(completeTemArray[index]);
     }
-    public void Open(tem item)
+
+    public void SmallOpen(int index)
     {
+        tem temm=new tem();
+        temm.index = ItemData.GetItem(element.SmallItemIndex[index]).index;
+        temm.type = ItemData.GetItem(element.SmallItemIndex[index]).type;
+        temm.ItemDescription = ItemData.GetItem(element.SmallItemIndex[index]).ItemDescription;
+        temm.ItemName= ItemData.GetItem(element.SmallItemIndex[index]).ItemName;
+        temm.ItemSprite = ItemData.GetItem(element.SmallItemIndex[index]).ItemSprite;
+        temm.CompleteItemIndex = ItemData.GetItem(element.SmallItemIndex[index]).CompleteItemIndex;
+        temm.WhereGet = ItemData.GetItem(element.SmallItemIndex[index]).WhereGet;
+        temm.SmallItemIndex = ItemData.GetItem(element.SmallItemIndex[index]).SmallItemIndex;
+        Open(temm);
+    }
+
+    public void Open(tem taaaaam)
+    {
+        element = taaaaam;
         anim.Play("InvenOpen");
         
-        for (int i = 0; i < item.CompleteItemIndex.Length; i++)
+        for (int i = 0; i < element.CompleteItemIndex.Length; i++)
         {
-            completeTemArray[i].index = ItemData.GetItem(item.CompleteItemIndex[i]).index;
-            completeTemArray[i].type = ItemData.GetItem(item.CompleteItemIndex[i]).type;
-            completeTemArray[i].ItemDescription = ItemData.GetItem(item.CompleteItemIndex[i]).ItemDescription;
-            completeTemArray[i].ItemName = ItemData.GetItem(item.CompleteItemIndex[i]).ItemName;
-            completeTemArray[i].ItemSprite = ItemData.GetItem(item.CompleteItemIndex[i]).ItemSprite;
-            completeTemArray[i].CompleteItemIndex = ItemData.GetItem(item.CompleteItemIndex[i]).CompleteItemIndex;
-            completeTemArray[i].WhereGet = ItemData.GetItem(item.CompleteItemIndex[i]).WhereGet;
-            completeTemArray[i].SmallItemIndex = ItemData.GetItem(item.CompleteItemIndex[i]).SmallItemIndex;
+            completeTemArray[i].index = ItemData.GetItem(element.CompleteItemIndex[i]).index;
+            completeTemArray[i].type = ItemData.GetItem(element.CompleteItemIndex[i]).type;
+            completeTemArray[i].ItemDescription = ItemData.GetItem(element.CompleteItemIndex[i]).ItemDescription;
+            completeTemArray[i].ItemName = ItemData.GetItem(element.CompleteItemIndex[i]).ItemName;
+            completeTemArray[i].ItemSprite = ItemData.GetItem(element.CompleteItemIndex[i]).ItemSprite;
+            completeTemArray[i].CompleteItemIndex = ItemData.GetItem(element.CompleteItemIndex[i]).CompleteItemIndex;
+            completeTemArray[i].WhereGet = ItemData.GetItem(element.CompleteItemIndex[i]).WhereGet;
+            completeTemArray[i].SmallItemIndex = ItemData.GetItem(element.CompleteItemIndex[i]).SmallItemIndex;
         }
 
-        for (int i = item.CompleteItemIndex.Length; i < completeTemArray.Length; i++)
+        for (int i = element.CompleteItemIndex.Length; i < completeTemArray.Length; i++)
         {
             completeTemArray[i].index = 0;
         }
@@ -62,41 +77,42 @@ public class Invent : MonoBehaviour
         {
             if (completeTemArray[i].index != 0)
             {
-                completes[i].sprite = completeTemArray[i].ItemSprite; 
-                completeBoxes[i].sprite = BoxSprite;
+                completeBoxes[i].gameObject.SetActive(true);
+                completes[i].gameObject.SetActive(true);
+                completes[i].sprite = completeTemArray[i].ItemSprite;
             }
             else
             {
-                completes[i].sprite = NoneSprite;
-                completeBoxes[i].sprite = NoneSprite;
+                completes[i].gameObject.SetActive(false);
+                completeBoxes[i].gameObject.SetActive(false);
             }
             
         }
 
-        if (item != null)
+        if (element != null)
         {
-            if (item.SmallItemIndex.Length >= 2)
+            if (element.SmallItemIndex.Length >= 2)
             {
                 Big.SetActive(false);
                 Small.SetActive(true);
-                SmallItemImg1.sprite = ItemData.GetItem(item.SmallItemIndex[0]).ItemSprite;
-                SmallItemImg2.sprite = ItemData.GetItem(item.SmallItemIndex[1]).ItemSprite;
-                SmallItemName1.text = ItemData.GetItem(item.SmallItemIndex[0]).ItemName;
-                SmallItemName2.text = ItemData.GetItem(item.SmallItemIndex[1]).ItemName;
-                BigItemImg2.sprite = item.ItemSprite;
-                BigItemName2.text = item.ItemName;
+                SmallItemImg1.sprite = ItemData.GetItem(element.SmallItemIndex[0]).ItemSprite;
+                SmallItemImg2.sprite = ItemData.GetItem(element.SmallItemIndex[1]).ItemSprite;
+                SmallItemName1.text = ItemData.GetItem(element.SmallItemIndex[0]).ItemName;
+                SmallItemName2.text = ItemData.GetItem(element.SmallItemIndex[1]).ItemName;
+                BigItemImg2.sprite = element.ItemSprite;
+                BigItemName2.text = element.ItemName;
             }
             else
             {
                 Big.SetActive(true);
                 Small.SetActive(false);
-                BigItemImg.sprite = item.ItemSprite;
-                BigItemName.text = item.ItemName;
+                BigItemImg.sprite = element.ItemSprite;
+                BigItemName.text = element.ItemName;
             }
-            ItemName.text = item.ItemName;
-            ItemDescription.text = item.ItemDescription;
+            ItemName.text = element.ItemName;
+            ItemDescription.text = element.ItemDescription;
           
-            WhereGet.text ="획득 경로 : "+ item.WhereGet;
+            WhereGet.text ="획득 경로 : "+ element.WhereGet;
         }
     }
 }
