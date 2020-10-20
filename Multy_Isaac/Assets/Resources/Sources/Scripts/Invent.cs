da7eba0c1a3eb1dcb6a5c8e274399446001ab61a
SexyM <48942066+SexyMg@users.noreply.github.com>
2020-10-20 11:27:39 +0900

    Revert "최종템이 좀 거슬리지만 진도 더 나갔다!"
    This reverts commit e07239590cce9f03cea3714ab9644107c4399cba.

--- Multy_Isaac/Assets/Resources/Sources/Scripts/Invent.cs
+++ Multy_Isaac/Assets/Resources/Sources/Scripts/Invent.cs
@@ -6,13 +6,14 @@ using UnityEngine.UI;
 
 public class Invent : MonoBehaviour
 {
+    public tem[] completeTemArray;
     private tem elementItem;
     public Sprite NoneSprite;
     public Sprite BoxSprite;
     public Image BigItemImg;
     public Text BigItemName;
     public Image[] completeBoxes; //조합템 테두리
-    public element[] completes;
+    public Image[] completes; //조합템
     public Text ItemName; //템이름 
     public Text ItemDescription; //템설명
     private ItemData ItemData;
@@ -20,55 +21,60 @@ public class Invent : MonoBehaviour
     private void Start()
     {
         ItemData = FindObjectOfType<ItemData>();
+        completeTemArray[0] = ItemData.temDatas[2]; //오류 하나 내줘서 첨에 제대로 안나오는거 고침 ㅋ
         anim = GetComponent<Animator>();
     }
-    
+
+    private void Update()
+    {
+        for (int i = 0; i < completeTemArray.Length; i++)
+        {
+            if (completeTemArray[i].index != 0)
+            {
+                completes[i].sprite = completeTemArray[i].ItemSprite; 
+                completeBoxes[i].sprite = BoxSprite;
+            }
+            else
+            {
+                completes[i].sprite = NoneSprite;
+                completeBoxes[i].sprite = NoneSprite;
+            }
+            
+        }
+
+        if (elementItem != null)
+        {
+            ItemName.text = elementItem.ItemName;
+            ItemDescription.text = elementItem.ItemDescription;
+            BigItemImg.sprite = elementItem.ItemSprite;
+            BigItemName.text = elementItem.ItemName;
+        }
+    }
+
     public void Close()
     {
         anim.Play("InvenClose");
     }
-    public void Open(int itemIndex)
+    public void Open(tem item)
     {
         anim.Play("InvenOpen");
-        foreach (tem tem in ItemData.temDatas)
+        elementItem = item;
+        for (int i = 0; i < item.CompleteItemIndex.Length; i++)
         {
-            if (tem.index == itemIndex)
+            foreach (tem tem in ItemData.temDatas)
             {
-                elementItem = tem;
-                break;
+                if (tem.index == item.CompleteItemIndex[i])
+                {
+                    completeTemArray[i] = ItemData.temDatas[item.CompleteItemIndex[i]];
+                    break;
+                }
             }
+           
         }
-        
-            for (int i = 0; i < elementItem.CompleteItemIndex.Length; i++)
-            {
-                foreach (tem tem in ItemData.temDatas)
-                {
-                    if (tem.index == elementItem.CompleteItemIndex[i])
-                    {
-                        completes[i].tem = tem;
-                        break;
-                    }
-                }
-            }
-            for (int i = 0; i < completes.Length; i++)
-            {
-                if (completes[i].tem.index != 0)
-                {
-                    completeBoxes[i].sprite = BoxSprite;
-                }
-                else
-                {
-                    completeBoxes[i].sprite = NoneSprite;
-                }
-            
-            }
 
-            if (elementItem != null)
-            {
-                ItemName.text = elementItem.ItemName;
-                ItemDescription.text = elementItem.ItemDescription;
-                BigItemImg.sprite = elementItem.ItemSprite;
-                BigItemName.text = elementItem.ItemName;
-            }
+        for (int i = item.CompleteItemIndex.Length; i < completeTemArray.Length; i++)
+        {
+            completeTemArray[i].index = 0;
+        }
     }
 }
