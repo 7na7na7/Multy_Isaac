e07239590cce9f03cea3714ab9644107c4399cba
SexyM <48942066+SexyMg@users.noreply.github.com>
2020-10-20 10:25:14 +0900

    최종템이 좀 거슬리지만 진도 더 나갔다!

--- Multy_Isaac/Assets/Resources/Sources/Scripts/Invent.cs
+++ Multy_Isaac/Assets/Resources/Sources/Scripts/Invent.cs
@@ -6,14 +6,13 @@ using UnityEngine.UI;
 
 public class Invent : MonoBehaviour
 {
-    public tem[] completeTemArray;
     private tem elementItem;
     public Sprite NoneSprite;
     public Sprite BoxSprite;
     public Image BigItemImg;
     public Text BigItemName;
     public Image[] completeBoxes; //조합템 테두리
-    public Image[] completes; //조합템
+    public element[] completes;
     public Text ItemName; //템이름 
     public Text ItemDescription; //템설명
     private ItemData ItemData;
@@ -21,60 +20,55 @@ public class Invent : MonoBehaviour
     private void Start()
     {
         ItemData = FindObjectOfType<ItemData>();
-        completeTemArray[0] = ItemData.temDatas[2]; //오류 하나 내줘서 첨에 제대로 안나오는거 고침 ㅋ
         anim = GetComponent<Animator>();
     }
-
-    private void Update()
-    {
-        for (int i = 0; i < completeTemArray.Length; i++)
-        {
-            if (completeTemArray[i].index != 0)
-            {
-                completes[i].sprite = completeTemArray[i].ItemSprite; 
-                completeBoxes[i].sprite = BoxSprite;
-            }
-            else
-            {
-                completes[i].sprite = NoneSprite;
-                completeBoxes[i].sprite = NoneSprite;
-            }
-            
-        }
-
-        if (elementItem != null)
-        {
-            ItemName.text = elementItem.ItemName;
-            ItemDescription.text = elementItem.ItemDescription;
-            BigItemImg.sprite = elementItem.ItemSprite;
-            BigItemName.text = elementItem.ItemName;
-        }
-    }
-
+    
     public void Close()
     {
         anim.Play("InvenClose");
     }
-    public void Open(tem item)
+    public void Open(int itemIndex)
     {
         anim.Play("InvenOpen");
-        elementItem = item;
-        for (int i = 0; i < item.CompleteItemIndex.Length; i++)
+        foreach (tem tem in ItemData.temDatas)
         {
-            foreach (tem tem in ItemData.temDatas)
+            if (tem.index == itemIndex)
             {
-                if (tem.index == item.CompleteItemIndex[i])
+                elementItem = tem;
+                break;
+            }
+        }
+        
+            for (int i = 0; i < elementItem.CompleteItemIndex.Length; i++)
+            {
+                foreach (tem tem in ItemData.temDatas)
                 {
-                    completeTemArray[i] = ItemData.temDatas[item.CompleteItemIndex[i]];
-                    break;
+                    if (tem.index == elementItem.CompleteItemIndex[i])
+                    {
+                        completes[i].tem = tem;
+                        break;
+                    }
                 }
             }
-           
-        }
+            for (int i = 0; i < completes.Length; i++)
+            {
+                if (completes[i].tem.index != 0)
+                {
+                    completeBoxes[i].sprite = BoxSprite;
+                }
+                else
+                {
+                    completeBoxes[i].sprite = NoneSprite;
+                }
+            
+            }
 
-        for (int i = item.CompleteItemIndex.Length; i < completeTemArray.Length; i++)
-        {
-            completeTemArray[i].index = 0;
-        }
+            if (elementItem != null)
+            {
+                ItemName.text = elementItem.ItemName;
+                ItemDescription.text = elementItem.ItemDescription;
+                BigItemImg.sprite = elementItem.ItemSprite;
+                BigItemName.text = elementItem.ItemName;
+            }
     }
 }
