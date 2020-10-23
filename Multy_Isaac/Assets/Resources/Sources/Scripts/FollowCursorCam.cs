ae68298f788da17ac203342c2255f0c003aa2fff
SexyM <48942066+SexyMg@users.noreply.github.com>
2020-10-23 19:25:10 +0900

    ㅎ/

---
+++ Multy_Isaac/Assets/Resources/Sources/Scripts/FollowCursorCam.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCursorCam : MonoBehaviour
+{
+    private void Update()
+    {
+        Vector2 pointer = Input.mousePosition.normalized;
+        print(pointer);
+    }
+}
