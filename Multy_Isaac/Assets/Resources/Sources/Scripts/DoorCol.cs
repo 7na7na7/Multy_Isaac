392caf0031da2abb005b36e76be8f939dd1faa5e
SexyM <48942066+SexyMg@users.noreply.github.com>
2020-10-28 12:14:16 +0900

    좀더 다듬자!

---
+++ Multy_Isaac/Assets/Resources/Sources/Scripts/DoorCol.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Photon.Pun;
+using UnityEngine;
+
+public class DoorCol : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player")&&other.GetComponent<PhotonView>().IsMine)
+        {
+            Camera.main.transform.position=new Vector3(transform.parent.transform.position.x,transform.parent.transform.position.y,-10);
+        }
+    }
+}
