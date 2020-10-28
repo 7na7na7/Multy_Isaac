e625f86169376f7f9f34cc21e45ee8d206799c46
SexyM <48942066+SexyMg@users.noreply.github.com>
2020-10-28 16:19:03 +0900

    혼자할땐 잘된다. 포톤네트워크 방에도 각각 넣어줘보자

--- Multy_Isaac/Assets/Resources/Sources/Scripts/Spawner.cs
+++ Multy_Isaac/Assets/Resources/Sources/Scripts/Spawner.cs
@@ -8,32 +8,12 @@ using Random = UnityEngine.Random;
 
 public class Spawner : MonoBehaviour
 {
-    public string monster;
-    public GameObject offlineMonster;
-    public float minDelay, maxDelay;
-    public BoxCollider2D bound;
-
     private void Start()
     {
-        StartCoroutine(spawn());
-    }
-
-    IEnumerator spawn()
-    {
-        while (true)
+        if (PhotonNetwork.IsMasterClient)
         {
-            yield return new WaitForSeconds(Random.Range(minDelay,maxDelay));
-            if (PhotonNetwork.IsMasterClient)
-            {
-                if (PhotonNetwork.OfflineMode)
-                    Instantiate(offlineMonster,
-                        new Vector3(Random.Range(bound.bounds.min.x, bound.bounds.max.x),
-                            Random.Range(bound.bounds.min.y, bound.bounds.max.y)), quaternion.identity);
-                else
-                    PhotonNetwork.InstantiateRoomObject(monster,
-                        new Vector3(Random.Range(bound.bounds.min.x, bound.bounds.max.x),
-                            Random.Range(bound.bounds.min.y, bound.bounds.max.y)), quaternion.identity);
-            }
+            PhotonNetwork.InstantiateRoomObject("StartEntry", transform.position, quaternion.identity);
         }
     }
+    
 }
