6bfafab8c3ed326d437e00c7fea18aafd870e8ff
SexyM <48942066+SexyMg@users.noreply.github.com>
2020-10-28 15:55:48 +0900

    콜라조정

--- Multy_Isaac/Assets/Resources/Sources/Scripts/Dungeon/RoomSpawner.cs
+++ Multy_Isaac/Assets/Resources/Sources/Scripts/Dungeon/RoomSpawner.cs
@@ -202,7 +202,7 @@ public class RoomSpawner : MonoBehaviour
         }
     }
 
-    private void OnTriggerStay2D(Collider2D other)
+    private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("SpawnPoint"))
         {
