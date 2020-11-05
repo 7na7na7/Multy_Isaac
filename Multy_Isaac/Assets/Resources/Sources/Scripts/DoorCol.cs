bf4c16e767d04c6e213c254ab7137c751a562b86
SexyM <48942066+SexyMg@users.noreply.github.com>
2020-11-05 22:58:27 +0900

    11_05_미니맵으로 섹스하는 법! 다음할거는 플레이어대가리 ㅍ시

--- Multy_Isaac/Assets/Resources/Sources/Scripts/DoorCol.cs
+++ Multy_Isaac/Assets/Resources/Sources/Scripts/DoorCol.cs
@@ -2,10 +2,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using Photon.Pun;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class DoorCol : MonoBehaviour
 {
+    private bool isInstantiate = false;
+    
+    public GameObject MinimapRoomPrefab;
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player")&&other.GetComponent<PhotonView>().IsMine)
@@ -13,6 +17,18 @@ public class DoorCol : MonoBehaviour
             Camera.main.transform.position=new Vector3(transform.parent.transform.position.x,transform.parent.transform.position.y,-10);
             Vector2 pos = transform.parent.transform.position;
             print(pos+" "+transform.parent.name.Substring(0, transform.parent.name.IndexOf("("))+"입니당!"); //(Clone) 앞까지 추출
+            
+            int x = (int)pos.x / 18;
+            int y = (int) pos.y / 10;
+            Vector3 minimapPos=new Vector3(500+x*0.9f,500+y*0.55f,-10);
+
+            if (isInstantiate==false)
+            {
+                Instantiate(MinimapRoomPrefab,new Vector3(minimapPos.x,minimapPos.y,0), quaternion.identity);
+                isInstantiate = true;
+            }
+
+            GameObject.FindGameObjectWithTag("Minimap").transform.position = minimapPos;
         }
     }
 }
--- Multy_Isaac/Assets/Resources/Sources/Scripts/Dungeon/AddRoom.cs
+++ Multy_Isaac/Assets/Resources/Sources/Scripts/Dungeon/AddRoom.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 
 public class AddRoom : MonoBehaviour
 {
+  //public Sprite minimapRoom;
   private RoomTemplates templates;
 
   private void Start()
