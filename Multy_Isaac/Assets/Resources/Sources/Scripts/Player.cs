04294c46b32cd5f51c565aeaed7dec252a8c4c46
SexyM <48942066+SexyMg@users.noreply.github.com>
2020-11-26 12:16:59 +0900

    클릭시 재장전으로 get에서 getdown

--- Multy_Isaac/Assets/Resources/Sources/Scripts/Player.cs
+++ Multy_Isaac/Assets/Resources/Sources/Scripts/Player.cs
@@ -208,18 +208,23 @@ public class Player : MonoBehaviourPunCallbacks, IPunObservable
                         
                         if (Input.GetKeyDown(KeyCode.R) && gun.activeSelf && leftBullet.isBulletMax()==false) //총 착용중이고, 총알이 꽉차지 않았고, R키를 눌렀을 시 재장전
                             reLoad(leftBullet.reLoadTime);
-                        if (Input.GetMouseButton(0) && gun.activeSelf && !isReLoading) //총쏘기
+
+                        if (Input.GetMouseButtonDown(0) && gun.activeSelf && !isReLoading)
                         {
-                            if(leftBullet.canShoot())
-                                ShotGun();
-                            else
+                            if (!leftBullet.canShoot())
                             {
-                                speed = savedSpeed;
                                 if(leftBullet.canReload())
                                     reLoad(leftBullet.reLoadTime);
                                 else
                                     print("총알이 부족합니다!");
                             }
+                        }
+                        if (Input.GetMouseButton(0) && gun.activeSelf && !isReLoading) //총쏘기
+                        {
+                            if (leftBullet.canShoot())
+                                ShotGun();
+                            else
+                                speed = savedSpeed;
                         }
                           
                         if (Input.GetMouseButtonUp(0)) //버튼에서 손을 떼면 원래속도로 돌아오기
