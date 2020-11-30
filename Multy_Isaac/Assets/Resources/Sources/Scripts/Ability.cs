c5590bbf5131fa28a243a8abf6fea8b10b41cbbe
SexyM <48942066+SexyMg@users.noreply.github.com>
2020-11-30 19:07:01 +0900

    어빌리티

---
+++ Multy_Isaac/Assets/Resources/Sources/Scripts/Ability.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum AbilityType { speedUp, dashDistanceUp, lasorPoint, critical, fullHeal}
+
+public class Ability : MonoBehaviour
+{
+    public bool isSpeedUp, isDashDistanceUp, isCritical;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
