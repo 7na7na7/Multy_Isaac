cb61127d049dc10ec9676bc03e8a18d2fe0bac36
SexyM <48942066+SexyMg@users.noreply.github.com>
2020-11-30 11:12:32 +0900

    사운드매니저_안겹치게 소스 돌려가며쓰기!

--- Multy_Isaac/Assets/Resources/Sources/Scripts/SoundManager.cs
+++ Multy_Isaac/Assets/Resources/Sources/Scripts/SoundManager.cs
@@ -1,21 +1,57 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class SoundManager : MonoBehaviour
 {
    public static SoundManager instance;
-  
-      private void Awake()
-      {
-          if (instance == null)
-          {
-              instance = this;
-              DontDestroyOnLoad(gameObject);
-          }
-          else
-          {
-              Destroy(gameObject);
-          }
-      }
+
+   public AudioClip[] clips;
+   private AudioSource[] ClipSources;
+   public AudioSource BgmSource;
+
+   private int index = 0;
+   private int length;
+   
+   private void Awake()
+   {
+       if (instance == null)
+       {
+           instance = this;
+           DontDestroyOnLoad(gameObject);
+       }
+       else
+       {
+           Destroy(gameObject);
+       }
+       
+       ClipSources=new AudioSource[clips.Length];
+       for (int i = 0; i < clips.Length; i++)
+       {
+           ClipSources[i] = gameObject.AddComponent<AudioSource>();
+           ClipSources[i].Stop();
+       }
+       
+       for (int j = 0; j < clips.Length; j++)
+       {
+           ClipSources[j].loop = false;
+           ClipSources[j].playOnAwake = false;
+       }
+
+       length = clips.Length;
+   }
+
+
+   public void Play( int clipIndex )
+   {
+       if (index == length)
+           index = 0;
+       
+       ClipSources[index].PlayOneShot(clips[clipIndex]);
+       print(index);
+          
+       index++;
+   }
+   
 }
