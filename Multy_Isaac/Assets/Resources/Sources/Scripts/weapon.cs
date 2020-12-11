c7bab03084eca507463bf6588cacfeac2ac85647
SexyM <48942066+SexyMg@users.noreply.github.com>
2020-12-11 23:46:00 +0900

    대충 무기하나 샘플로 만들고 팔자식에총넣었다! 잘 회전시켜보자...

--- Multy_Isaac/Assets/Resources/Sources/Scripts/weapon.cs
+++ Multy_Isaac/Assets/Resources/Sources/Scripts/weapon.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 [System.Serializable]
 public class wep
 {
+    public bool isMelee = false;
     public Sprite spr;
     public Vector2 tr;
     public Vector2 scale;
@@ -18,6 +19,7 @@ public class wep
     public wep DeepCopy()
     {
         wep Copytem = new wep();
+        Copytem.isMelee = this.isMelee;
         Copytem.reLoadTime = this.reLoadTime;
         Copytem.BulletCount = this.BulletCount;
         Copytem.spr = this.spr;
