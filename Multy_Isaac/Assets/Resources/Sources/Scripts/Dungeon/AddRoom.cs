48e84952ae2e1d3c0d7ba8acee969e6eecd0c81b
SexyM <48942066+SexyMg@users.noreply.github.com>
2020-12-23 15:24:03 +0900

    큰방도 전부 맵프로퍼티 생성되도록 변경, 버그는 0.2초 코루틴으로 해결
    이제 맵 프로퍼티에 쓸 오브젝트 프리팹?

--- Multy_Isaac/Assets/Resources/Sources/Scripts/Dungeon/AddRoom.cs
+++ Multy_Isaac/Assets/Resources/Sources/Scripts/Dungeon/AddRoom.cs
@@ -27,28 +27,78 @@ public class AddRoom : MonoBehaviour
       if(templates.GetComponent<PhotonView>().IsMine) 
         templates.rooms.Add(this.gameObject); 
     }
-    
-    if (specialvalue != -1)
+
+    int a = 0;
+    for (int i = 0; i < transform.childCount; i++)
     {
-      if (PhotonNetwork.OfflineMode)
+      if (transform.GetChild(i).GetComponent<RoomSpawner>())
+        a++;
+    }
+   
+    if (specialvalue != -1) //특별한 방이 아니면
+    {
+      if (a >= 4) //클방이면
       {
-        Instantiate(templates.SpecialRooms[specialvalue], transform.position,quaternion.identity);
+        for (int i = 0; i < transform.childCount; i++)
+        {
+          if (transform.GetChild(i).GetComponent<RoomSpawner>())
+          {
+            if (PhotonNetwork.OfflineMode)
+              Instantiate(templates.SpecialRooms[specialvalue], transform.GetChild(i).transform.position, quaternion.identity);
+            else
+              PhotonNetwork.InstantiateRoomObject(templates.SpecialRooms[specialvalue].name, transform.GetChild(i).transform.position, quaternion.identity);
+          }
+        } 
       }
-      else
+      else //작은방이면
       {
-        PhotonNetwork.InstantiateRoomObject(templates.SpecialRooms[specialvalue].name, transform.position,quaternion.identity);
+        if (PhotonNetwork.OfflineMode)
+        {
+          Instantiate(templates.SpecialRooms[specialvalue], transform.position,quaternion.identity);
+        }
+        else
+        {
+          PhotonNetwork.InstantiateRoomObject(templates.SpecialRooms[specialvalue].name, transform.position,quaternion.identity);
+        } 
       }
     }
-    else
+    else //특별한 방이면
     {
-      if (PhotonNetwork.OfflineMode)
+      if (a >= 4) //큰방이면
       {
-        Instantiate(templates.RoomProps[Random.Range(0,templates.RoomProps.Length)], transform.position,quaternion.identity);
+        for (int i = 0; i < transform.childCount; i++)
+        {
+          if (transform.GetChild(i).GetComponent<RoomSpawner>())
+          {
+            if (PhotonNetwork.OfflineMode)
+              StartCoroutine(Spawn(templates.RoomProps[Random.Range(0, templates.RoomProps.Length)], transform.GetChild(i).transform.position, quaternion.identity));
+            else
+              StartCoroutine(Spawn_P(templates.RoomProps[Random.Range(0, templates.RoomProps.Length)].name, transform.GetChild(i).transform.position, quaternion.identity));
+          }
+        } 
       }
-      else
+      else //작은방이면
       {
-        PhotonNetwork.InstantiateRoomObject(templates.RoomProps[Random.Range(0,templates.RoomProps.Length)].name, transform.position,quaternion.identity);
+        if (PhotonNetwork.OfflineMode)
+        {
+          StartCoroutine(Spawn(templates.RoomProps[Random.Range(0, templates.RoomProps.Length)], transform.position, quaternion.identity));
+        }
+        else
+        {
+          StartCoroutine(Spawn_P(templates.RoomProps[Random.Range(0, templates.RoomProps.Length)].name, transform.position, quaternion.identity));
+        }
       }
     }
+  }
+  IEnumerator Spawn(GameObject go, Vector3 pos, quaternion q)
+  {
+    yield return new WaitForSeconds(0.2f);
+    Instantiate(go, pos, q);
+  }
+  
+  IEnumerator Spawn_P(string go,Vector3 pos,quaternion q)
+  {
+    yield return new WaitForSeconds(0.2f);
+    PhotonNetwork.Instantiate(go, pos, q);
   }
 }
