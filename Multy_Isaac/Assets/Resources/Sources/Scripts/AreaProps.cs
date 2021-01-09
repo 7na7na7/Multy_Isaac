66e8ff1abb3c9387d093f49b440ae5e51e837b6c
SexyM <48942066+SexyMg@users.noreply.github.com>
2021-01-09 13:57:00 +0900

    데코레이션

--- Multy_Isaac/Assets/Resources/Sources/Scripts/AreaProps.cs
+++ Multy_Isaac/Assets/Resources/Sources/Scripts/AreaProps.cs
@@ -1,18 +1,27 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class AreaProps : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject[] decos;
+    public int minDeco;
+    public int maxDeco;
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        if (decos.Length != 0)
+        {
+            int decoCount = Random.Range(minDeco, maxDeco);
+
+            for (int i = 0; i < decoCount; i++)
+            {
+                Instantiate(decos[Random.Range(0, decos.Length)],
+                    transform.position+new Vector3(Random.Range(-9f, 9f), Random.Range(-5f,4.8f), 0), quaternion.identity); //벽이랑 안겹치게 0.2f떨어뜨려줌
+            }
+        }
     }
 }
