ed8c04fbfa4e85603a985fc36221876789996970
SexyM <48942066+SexyMg@users.noreply.github.com>
2021-01-16 22:33:04 +0900

    슬라임 AI간단하게제작중

--- Multy_Isaac/Assets/Resources/Sources/Scripts/Monster/Slime.cs
+++ Multy_Isaac/Assets/Resources/Sources/Scripts/Monster/Slime.cs
@@ -1,18 +1,49 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using CodeMonkey.Utils;
+using Random = UnityEngine.Random;
 
 public class Slime : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float minMove;
+    public float maxMove;
+    public float speed;
+    private Rigidbody2D rigid;
+    private Vector3 startingPosition;
+   private Vector3 roamingPosition;
+   private void Start()
+   {
+       rigid = GetComponent<Rigidbody2D>();
+      startingPosition = transform.position;
+      roamingPosition = GetRoamingPosition();
+      roamingPosition = GetRoamingPosition();
+   }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   private void Update()
+   {
+       Vector2 dir = (roamingPosition - transform.position).normalized;
+       rigid.velocity = dir * speed;
+
+       float reachedPositionDistance = 1f;
+       if (Vector2.Distance(transform.position, roamingPosition) < reachedPositionDistance)
+       {
+           //지정한 곳에 도착했으니 다시 값 줌
+           roamingPosition = GetRoamingPosition();
+       }
+   }
+
+   private Vector3 GetRoamingPosition()
+   {
+       float randomMove = Random.Range(minMove, maxMove);
+       print(randomMove);
+       return startingPosition + UtilsClass.GetRandomDir() * randomMove;
+   }
+
+   private void OnCollisionStay2D(Collision2D other)
+   {
+       if (other.gameObject.CompareTag("Wall"))
+           roamingPosition = GetRoamingPosition();
+   }
 }
