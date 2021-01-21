36de96bd9cac8a5d70b15ec6e087b5a562f8d90d
SexyM <48942066+SexyMg@users.noreply.github.com>
2021-01-21 13:04:24 +0900

    슬라임 동기화

--- Multy_Isaac/Assets/Resources/Sources/Scripts/AreaProps.cs
+++ Multy_Isaac/Assets/Resources/Sources/Scripts/AreaProps.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using Photon.Pun;
 using Unity.Mathematics;
 using UnityEngine;
 using Random = UnityEngine.Random;
@@ -13,17 +14,38 @@ public class AreaProps : MonoBehaviour
 
     private void Start()
     {
-        
-       
-            if (decos.Length != 0)
+        if (PhotonNetwork.OfflineMode)
+        {
+            setProps();
+        }
+        else
+        {
+            if (PhotonNetwork.IsMasterClient)
             {
-                int decoCount = Random.Range(minDeco, maxDeco);
+                setProps();
+            }
+        }
+    }
 
-                for (int i = 0; i < decoCount; i++)
+    void setProps()
+    {
+        if (decos.Length != 0)
+        {
+            int decoCount = Random.Range(minDeco, maxDeco);
+
+            for (int i = 0; i < decoCount; i++)
+            {
+                if (PhotonNetwork.OfflineMode)
                 {
                     GameObject go=Instantiate(decos[Random.Range(0, decos.Length)], transform);
                     go.transform.position = transform.position + new Vector3(Random.Range(-7.5f, 7.5f), Random.Range(-4f, 2.5f), 0); //벽이랑 안겹치게 1씩 떨어뜨려줌
                 }
+                else
+                {
+                    GameObject go=PhotonNetwork.InstantiateRoomObject(decos[Random.Range(0, decos.Length)].name, 
+                        transform.position + new Vector3(Random.Range(-7.5f, 7.5f), Random.Range(-4f, 2.5f), 0),quaternion.identity);
+                }
             }
+        }
     }
 }
