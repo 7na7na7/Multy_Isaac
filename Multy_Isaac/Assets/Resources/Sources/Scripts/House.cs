96f3a91e3e5b1b98310f935839ab4ea1c504d916
SexyM <48942066+SexyMg@users.noreply.github.com>
2021-01-22 22:49:58 +0900

    바닥추가!

--- Multy_Isaac/Assets/Resources/Sources/Scripts/House.cs
+++ Multy_Isaac/Assets/Resources/Sources/Scripts/House.cs
@@ -7,30 +7,39 @@ public class House : MonoBehaviour
 {
     public GameObject ver1;
     public GameObject ver2;
+    public GameObject ver3;
     void Start()
     {
         if (PhotonNetwork.OfflineMode)
         {
-            int r = Random.Range(0, 2);
+            int r = Random.Range(0, 3);
             if (r == 0)
                 ver1.SetActive(true);
-            else
+            else if(r==1)
             {
                 ver2.SetActive(true);
+            }
+            else
+            {
+                ver3.SetActive(true);
             }
         }
         else
         {
             if (PhotonNetwork.IsMasterClient)
             {
-                int r = Random.Range(0, 2);
+                int r = Random.Range(0, 3);
                 if (r == 0)
                 {
                     GetComponent<PhotonView>().RPC("set1",RpcTarget.All);
                 }
-                else
+                else if(r==1)
                 {
                     GetComponent<PhotonView>().RPC("set2",RpcTarget.All);
+                }
+                else
+                {
+                    GetComponent<PhotonView>().RPC("set3",RpcTarget.All);
                 }
             }
         }
@@ -45,5 +54,10 @@ public class House : MonoBehaviour
     void set2()
     {
         ver2.SetActive(true);
+    }
+    [PunRPC]
+    void set3()
+    {
+        ver3.SetActive(true);
     }
 }
