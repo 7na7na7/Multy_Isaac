49c6ea747f37177d0e59e114fca33e37df41276c
SexyM <48942066+SexyMg@users.noreply.github.com>
2021-01-27 18:13:02 +0900

    시체 좌우반전

--- Multy_Isaac/Assets/Resources/Sources/Scripts/ZombieSpawner.cs
+++ Multy_Isaac/Assets/Resources/Sources/Scripts/ZombieSpawner.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using Photon.Pun;
 using Unity.Mathematics;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class ZombieSpawner : MonoBehaviour
 {
+    public Vector2 randomMin, randomMax;
+    public int Count;
     public GameObject regularZombie;
   
     void Start()
@@ -17,7 +20,8 @@ public class ZombieSpawner : MonoBehaviour
     {
         if (PhotonNetwork.OfflineMode)
         {
-            Instantiate(regularZombie, transform.position, quaternion.identity);
+            for(int i=0;i<Count;i++)
+                Instantiate(regularZombie, new Vector2(Random.Range(randomMin.x,randomMax.x),Random.Range(randomMin.y,randomMax.y)), quaternion.identity);
         }
         else
         {
