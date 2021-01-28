95c518ae113d3528f95be0713fb8c9df4f2f0403
SexyM <48942066+SexyMg@users.noreply.github.com>
2021-01-28 16:52:54 +0900

    Prop셑

--- Multy_Isaac/Assets/Resources/Sources/Scripts/Roof.cs
+++ Multy_Isaac/Assets/Resources/Sources/Scripts/Roof.cs
@@ -4,18 +4,54 @@ using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
 using Photon.Pun;
+using Random = UnityEngine.Random;
 
+[System.Serializable]
+public class prop
+{
+    public int perValue;
+    public int index;
+    public Sprite roofSprite;
+    public int[] getItemIndex;
+    public GameObject[] properties;
+}
 public class Roof : MonoBehaviour
 {
+    public prop[] props;
+    private prop realProp;
     public float alpha = 0.01f;
     private SpriteRenderer spr;
-   
+    private List<int> indexList= new List<int>();
+    
     private void Start()
     {
         spr = GetComponent<SpriteRenderer>();
+        Set();
     }
 
+    void Set()
+    {
+        for (int i = 0; i < props.Length; i++)
+        {
+            for (int j = 0; j < props[i].perValue;j++)
+            {
+                indexList.Add(props[i].index);   
+            }
+        }
 
+        int index = indexList[Random.Range(0, indexList.Count)];
+
+        for (int i = 0; i < props.Length; i++)
+        {
+            if (props[i].index == index)
+            {
+                realProp = props[i];
+                break;
+            }
+        }
+
+        spr.sprite = realProp.roofSprite;
+    }
     void invisible()
     {
         Color color = spr.color;
