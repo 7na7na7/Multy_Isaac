4060047f75d07076bea3583cb969f7c03ebbafb2
SexyM <48942066+SexyMg@users.noreply.github.com>
2021-03-15 18:19:39 +0900

    터렛완성

--- Multy_Isaac/Assets/Resources/Sources/Scripts/Turret.cs
+++ Multy_Isaac/Assets/Resources/Sources/Scripts/Turret.cs
@@ -6,8 +6,10 @@ using UnityEngine;
 
 public class Turret : MonoBehaviour
 {
+    public float shotRadious;
     public GameObject TurretBullet;
     public float shotDelay = 2;
+    private Vector2 position;
     private void Start()
     {
         StartCoroutine(cor());
@@ -18,17 +20,54 @@ public class Turret : MonoBehaviour
         while (true)
         {
             yield return new WaitForSeconds(shotDelay);
-            if (PhotonNetwork.OfflineMode)
+            RaycastHit2D[] zombieCol = Physics2D.CircleCastAll(transform.position, shotRadious, Vector2.up,0);
+
+            bool canFind = false;
+            foreach (RaycastHit2D col in zombieCol)
             {
-                Instantiate(TurretBullet, transform.position, Quaternion.identity);
-            }
-            else
-            {
-                if (PhotonNetwork.IsMasterClient)
+                if (col.collider.CompareTag("Enemy"))
                 {
-                    PhotonNetwork.InstantiateRoomObject(TurretBullet.name, transform.position, Quaternion.identity);
+                    position = col.collider.transform.position;
+                    canFind = true;
+                    break;
                 }
             }
+
+            if (canFind)
+            {
+                Vector3 angle=new Vector3(0, 0, -getAngle(transform.position.x, transform.position.y, position.x, position.y)+90);
+                if (PhotonNetwork.OfflineMode)
+                {
+                    setAngleRPC(angle);
+                    Instantiate(TurretBullet, transform.position, Quaternion.Euler(angle));
+                }
+                else
+                {
+                    GetComponent<PhotonView>().RPC("setAngleRPC",RpcTarget.All);
+                    if (PhotonNetwork.IsMasterClient)
+                    {
+                        PhotonNetwork.InstantiateRoomObject(TurretBullet.name, transform.position, Quaternion.Euler(angle));
+                    }
+                }   
+            }
+            
         }
     }
+
+    [PunRPC]
+    void setAngleRPC(Vector3 angle)
+    {
+        transform.eulerAngles = angle;
+    }
+        private float getAngle(float x1, float y1, float x2, float y2) //Vector값을 넘겨받고 회전값을 넘겨줌
+        {
+            float dx = x2 - x1;
+            float dy = y2 - y1;
+
+            float rad = Mathf.Atan2(dx, dy);
+            float degree = rad * Mathf.Rad2Deg;
+        
+            return degree;
+        }
 }
+    
