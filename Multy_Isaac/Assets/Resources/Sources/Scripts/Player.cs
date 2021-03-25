65baffc7ca163c8929d52a4575d1d6035b04f5b8
SexyM <48942066+SexyMg@users.noreply.github.com>
2021-03-25 23:44:05 +0900

    닉오프라인에서도 잘보임, 게임빌드 이름과 아이콘 설정, 캐릭터셀렉트 잘보이게

--- Multy_Isaac/Assets/Resources/Sources/Scripts/Player.cs
+++ Multy_Isaac/Assets/Resources/Sources/Scripts/Player.cs
@@ -122,7 +122,8 @@ public class Player : MonoBehaviourPunCallbacks, IPunObservable
         temMgr = FindObjectOfType<TemManager>();
         nickname.text = pv.IsMine ? PhotonNetwork.NickName : pv.Owner.NickName; //닉네임 설정, 자기 닉네임이 아니면 상대 닉네임으로
         nickname.color = pv.IsMine ? Color.green : Color.red; //닉네임 색깔 설정, 자기 닉네임이면 초록색, 아니면 빨강색
-        
+        if (PhotonNetwork.OfflineMode)
+            nickname.text = PlayerPrefs.GetString("NameKey", "Player");
         anim = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
         localScaleX = transform.localScale.x;
