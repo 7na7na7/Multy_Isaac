90ba72bb7e4a6a6e158879b4bea72d89072578ae
SexyM <48942066+SexyMg@users.noreply.github.com>
2021-03-25 22:50:06 +0900

    플레이어바꾸기 완벽연동 완료!

---
+++ Multy_Isaac/Assets/Resources/Sources/Scripts/PlayerChange.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerChange : MonoBehaviour
+{
+    public int Index = 0;
+    public Sprite[] sprites;
+    public Sprite NoneSprite;
+    public Image leftImg;
+    public Image mainImg;
+    public Image rightImg;
+    private string playerKey = "playerKey";
+
+    private void Start()
+    {
+        playerCountSave.instance.PlayerIndex = PlayerPrefs.GetInt(playerKey, 0);
+      Index = playerCountSave.instance.PlayerIndex;
+      mainImg.sprite = sprites[Index];
+      if (Index == 0)
+      {
+          leftImg.sprite = sprites[sprites.Length-1];
+          rightImg.sprite = sprites[Index + 1];
+      }
+      else
+      {
+          rightImg.sprite = sprites[Index + 1];
+          leftImg.sprite = sprites[Index  -1];
+      }
+    }
+
+    public void Left()
+    {
+        Index--;
+        if (Index == 0)
+        {
+            leftImg.sprite = sprites[sprites.Length-1];
+            rightImg.sprite = sprites[Index + 1];
+        }
+        else if (Index == -1)
+        {
+            Index = sprites.Length - 1;
+            rightImg.sprite = sprites[0];
+            leftImg.sprite = sprites[Index  -1];
+        }
+        else
+        {
+            rightImg.sprite = sprites[Index + 1];
+            leftImg.sprite = sprites[Index  -1];
+        }
+        mainImg.sprite = sprites[Index];
+        PlayerPrefs.SetInt(playerKey,Index);
+        playerCountSave.instance.PlayerIndex = Index;
+    }
+
+    public void Right()
+    {
+        Index++;
+        if (Index == sprites.Length - 1)
+        {
+            rightImg.sprite =sprites[0];
+            leftImg.sprite = sprites[Index  -1];
+        }
+        else if (Index == sprites.Length)
+        {
+            Index = 0;
+            rightImg.sprite = sprites[1];
+            leftImg.sprite = sprites[sprites.Length-1];
+        }
+        else
+        {
+            rightImg.sprite = sprites[Index + 1];
+            leftImg.sprite = sprites[Index  -1];
+        }
+        mainImg.sprite = sprites[Index]; 
+        PlayerPrefs.SetInt(playerKey,Index);
+        playerCountSave.instance.PlayerIndex = Index;
+    }
+}
--- Multy_Isaac/Assets/Resources/Sources/Scripts/playerCountSave.cs
+++ Multy_Isaac/Assets/Resources/Sources/Scripts/playerCountSave.cs
@@ -6,8 +6,6 @@ using UnityEngine.UI;
 
 public class playerCountSave : MonoBehaviour
 {
-   public Image[] imgs;
-   private string playerKey = "playerKey";
    public int PlayerIndex = 0;
    public static playerCountSave instance;
    private void Awake()
@@ -23,45 +21,5 @@ public class playerCountSave : MonoBehaviour
       }
    }
 
-   private void Start()
-   {
-      PlayerIndex = PlayerPrefs.GetInt(playerKey, 0);
-      for (int i = 0; i < imgs.Length; i++)
-      {
-         if (i == PlayerIndex)
-         {
-            Color color= Color.white;
-            color.a = 1f;
-            imgs[i].color = color;
-         }
-         else
-         {
-            Color color= Color.white;
-            color.a = 0.5f;
-            imgs[i].color = color;
-         }
-      }
-   }
 
-   public void SetIndex(int index)
-   {
-      PlayerIndex = index;
-      PlayerPrefs.SetInt(playerKey,PlayerIndex);
-
-      for (int i = 0; i < imgs.Length; i++)
-      {
-         if (i == PlayerIndex)
-         {
-            Color color= Color.white;
-            color.a = 1f;
-            imgs[i].color = color;
-         }
-         else
-         {
-            Color color= Color.white;
-            color.a = 0.5f;
-            imgs[i].color = color;
-         }
-      }
-   }
 }
